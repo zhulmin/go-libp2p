efdf2d3bd6cb555d8f6661188a27c86e89e7cf60
Cole Brown <bigswim@gmail.com>
2019-05-17 15:18:32 -0400

    Add MemoryTransport

--- go.mod
+++ go.mod
@@ -1,13 +1,18 @@
 module github.com/libp2p/go-libp2p
 
 require (
+	github.com/glycerine/goconvey v0.0.0-20190410193231-58a59202ab31 // indirect
+	github.com/glycerine/rbuf v0.0.0-20190314090850-75b78581bebe
 	github.com/gogo/protobuf v1.2.1
+	github.com/gopherjs/gopherjs v0.0.0-20190430165422-3e4dfb77656c // indirect
 	github.com/ipfs/go-cid v0.0.2
 	github.com/ipfs/go-detect-race v0.0.1
 	github.com/ipfs/go-ipfs-util v0.0.1
 	github.com/ipfs/go-log v0.0.1
 	github.com/jbenet/go-cienv v0.1.0
 	github.com/jbenet/goprocess v0.1.3
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/libp2p/go-conn-security v0.1.0
 	github.com/libp2p/go-conn-security-multistream v0.1.0
 	github.com/libp2p/go-libp2p-autonat v0.1.0
 	github.com/libp2p/go-libp2p-blankhost v0.1.1
@@ -18,13 +23,16 @@ require (
 	github.com/libp2p/go-libp2p-mplex v0.2.1
 	github.com/libp2p/go-libp2p-nat v0.0.4
 	github.com/libp2p/go-libp2p-netutil v0.1.0
+	github.com/libp2p/go-libp2p-peer v0.2.0
 	github.com/libp2p/go-libp2p-peerstore v0.1.0
 	github.com/libp2p/go-libp2p-secio v0.1.0
 	github.com/libp2p/go-libp2p-swarm v0.1.0
 	github.com/libp2p/go-libp2p-testing v0.0.3
+	github.com/libp2p/go-libp2p-transport v0.1.0
 	github.com/libp2p/go-libp2p-transport-upgrader v0.1.1
 	github.com/libp2p/go-libp2p-yamux v0.2.0
 	github.com/libp2p/go-maddr-filter v0.0.4
+	github.com/libp2p/go-stream-muxer v0.1.0 // indirect
 	github.com/libp2p/go-stream-muxer-multistream v0.2.0
 	github.com/libp2p/go-tcp-transport v0.1.0
 	github.com/libp2p/go-ws-transport v0.1.0
@@ -33,5 +41,6 @@ require (
 	github.com/multiformats/go-multiaddr-dns v0.0.2
 	github.com/multiformats/go-multiaddr-net v0.0.1
 	github.com/multiformats/go-multistream v0.1.0
+	github.com/whyrusleeping/go-smux-multiplex v3.0.16+incompatible
 	github.com/whyrusleeping/mdns v0.0.0-20180901202407-ef14215e6b30
 )
---
+++ p2p/transport/mem/memorytransport_test.go
@@ -0,0 +1,96 @@
+package memorytransport
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	insecure "github.com/libp2p/go-conn-security/insecure"
+	peertest "github.com/libp2p/go-libp2p-peer/test"
+	tptu "github.com/libp2p/go-libp2p-transport-upgrader"
+	utils "github.com/libp2p/go-libp2p-transport/test"
+	ma "github.com/multiformats/go-multiaddr"
+	mplex "github.com/whyrusleeping/go-smux-multiplex"
+)
+
+func TestMemoryTransport(t *testing.T) {
+	id, err := peertest.RandPeerID()
+	if err != nil {
+		t.Fatal(err)
+	}
+	upgrader := &tptu.Upgrader{
+		Secure: insecure.New(id),
+		Muxer:  new(mplex.Transport),
+	}
+	transport := New(upgrader)
+	listenAddrStr := fmt.Sprintf("/ipfs/%s", id.Pretty())
+	listenAddr, _ := ma.NewMultiaddr(listenAddrStr)
+	listener, err := transport.Listen(listenAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		conn, err := listener.Accept()
+		if err != nil {
+			t.Fatal(err)
+		}
+		stream, err := conn.OpenStream()
+		if err != nil {
+			t.Fatal(err)
+		}
+		n, err := stream.Write([]byte("sup"))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if n != 3 {
+			t.Fatalf("expected to write 3 bytes, wrote %d", n)
+		}
+	}()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	conn, err := transport.Dial(ctx, listenAddr, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	stream, err := conn.AcceptStream()
+	if err != nil {
+		t.Fatal(err)
+	}
+	buf := make([]byte, 10)
+	n, err := stream.Read(buf)
+	if n != 3 {
+		t.Fatal("expected 3 bytes, got", n)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	bufStr := string(buf[0:n])
+	if bufStr != "sup" {
+		t.Fatal("unexpected message: ", bufStr)
+	}
+}
+
+// Note: the stress tests don't work because they rely on multiple listeners
+// being able to bind the same address, usually by requesting TCP port 0 from
+// the kernel. This doesn't apply for the in memory transport. The in memory
+// transport supports one per peer ID.
+func TestMemoryTransportFull(t *testing.T) {
+	id, err := peertest.RandPeerID()
+	if err != nil {
+		t.Fatal(err)
+	}
+	upgrader := &tptu.Upgrader{
+		Secure: insecure.New(id),
+		Muxer:  new(mplex.Transport),
+	}
+	transport := New(upgrader)
+	listenAddrStr := fmt.Sprintf("/ipfs/%s", id.Pretty())
+	listenAddr, _ := ma.NewMultiaddr(listenAddrStr)
+	utils.SubtestBasic(t, transport, transport, listenAddr, id)
+	utils.SubtestProtocols(t, transport, transport, listenAddr, id)
+	utils.SubtestPingPong(t, transport, transport, listenAddr, id)
+	utils.SubtestCancel(t, transport, transport, listenAddr, id)
+	utils.SubtestStreamReset(t, transport, transport, listenAddr, id)
+}
