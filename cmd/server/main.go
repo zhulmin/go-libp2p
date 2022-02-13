7b5e2b4387d3036adb901de5d8557b3be5c52fa0
Marten Seemann <martenseemann@gmail.com>
2022-02-13 17:42:02 +0400

    start TLS challenge endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"os"
@@ -11,11 +12,26 @@ import (
 )
 
 func main() {
+	certmagic.DefaultACME.Agreed = true
+	certmagic.DefaultACME.Email = "spam@spam.com"
 	certmagic.DefaultACME.CA = certmagic.LetsEncryptStagingCA
 	conf, err := certmagic.TLS([]string{os.Args[1]})
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	go func() {
+		cache := certmagic.NewCache(certmagic.CacheOptions{})
+		conf := certmagic.NewDefault()
+		magic := certmagic.New(cache, *conf)
+		tlsConfig := magic.TLSConfig()
+		ln, err := tls.Listen("tcp", ":443", tlsConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_ = ln
+	}()
+
 	host, _ := libp2p.New(
 		libp2p.Transport(ws.New, ws.WithTLSConfig(conf)),
 		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/1234/wss"),
