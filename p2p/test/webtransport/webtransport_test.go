949cd48fb271718b174cb56e20b81a9669305e4a
Marco Munizaga <git@marcopolo.io>
2022-10-21 15:23:23 +0100

    Use same key

--- p2p/test/webtransport/webtransport_test.go
+++ p2p/test/webtransport/webtransport_test.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/benbjohnson/clock"
 	"github.com/libp2p/go-libp2p"
+	ic "github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/test"
 	libp2pwebtransport "github.com/libp2p/go-libp2p/p2p/transport/webtransport"
 	ma "github.com/multiformats/go-multiaddr"
 	"github.com/stretchr/testify/require"
@@ -23,10 +25,13 @@ func extractCertHashes(addr ma.Multiaddr) []string {
 }
 
 func TestDeterministicCertsAfterReboot(t *testing.T) {
+	priv, _, err := test.RandTestKeyPair(ic.Ed25519, 256)
+	require.NoError(t, err)
+
 	cl := clock.NewMock()
 	// Move one year ahead to avoid edge cases around epoch
 	cl.Add(time.Hour * 24 * 365)
-	h, err := libp2p.New(libp2p.NoTransports, libp2p.Transport(libp2pwebtransport.New, libp2pwebtransport.WithClock(cl)))
+	h, err := libp2p.New(libp2p.NoTransports, libp2p.Transport(libp2pwebtransport.New, libp2pwebtransport.WithClock(cl)), libp2p.Identity(priv))
 	require.NoError(t, err)
 	err = h.Network().Listen(ma.StringCast("/ip4/127.0.0.1/udp/0/quic/webtransport"))
 	require.NoError(t, err)
@@ -34,7 +39,7 @@ func TestDeterministicCertsAfterReboot(t *testing.T) {
 	prevCerthashes := extractCertHashes(h.Addrs()[0])
 	h.Close()
 
-	h, err = libp2p.New(libp2p.NoTransports, libp2p.Transport(libp2pwebtransport.New, libp2pwebtransport.WithClock(cl)))
+	h, err = libp2p.New(libp2p.NoTransports, libp2p.Transport(libp2pwebtransport.New, libp2pwebtransport.WithClock(cl)), libp2p.Identity(priv))
 	require.NoError(t, err)
 	defer h.Close()
 	err = h.Network().Listen(ma.StringCast("/ip4/127.0.0.1/udp/0/quic/webtransport"))
