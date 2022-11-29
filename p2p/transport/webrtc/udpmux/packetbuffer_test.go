afd33f79982460b2364185c78445aa4bbe8e35a0
Chinmay Kousik <chinmaykousik1@gmail.com>
2022-11-29 15:39:12 +0530

    fix gofmt

--- p2p/transport/webrtc/udpmux/packetbuffer_test.go
+++ p2p/transport/webrtc/udpmux/packetbuffer_test.go
@@ -13,8 +13,8 @@ import (
 func TestPacketBuffer_QueuePacketsForRead(t *testing.T) {
 	ctx := context.Background()
 	b := newPacketBuffer(ctx)
-	b.writePacket([]byte{ 1, 2, 3 }, net.UDPAddrFromAddrPort(netip.MustParseAddrPort("127.0.0.1:1234"))) 
-	b.writePacket([]byte{ 5, 6, 7, 8 }, net.UDPAddrFromAddrPort(netip.MustParseAddrPort("127.0.0.1:1235"))) 
+	b.writePacket([]byte{1, 2, 3}, net.UDPAddrFromAddrPort(netip.MustParseAddrPort("127.0.0.1:1234")))
+	b.writePacket([]byte{5, 6, 7, 8}, net.UDPAddrFromAddrPort(netip.MustParseAddrPort("127.0.0.1:1235")))
 
 	buf := make([]byte, 100)
 	size, _, err := b.readFrom(buf)
@@ -31,8 +31,8 @@ func TestPacketBuffer_WaitsForData(t *testing.T) {
 	pb := newPacketBuffer(ctx)
 	buf := make([]byte, 100)
 
-	timer := time.AfterFunc(200 * time.Millisecond, func () {
-		pb.writePacket([]byte{ 5, 6, 7, 8 }, net.UDPAddrFromAddrPort(netip.MustParseAddrPort("127.0.0.1:1235"))) 
+	timer := time.AfterFunc(200*time.Millisecond, func() {
+		pb.writePacket([]byte{5, 6, 7, 8}, net.UDPAddrFromAddrPort(netip.MustParseAddrPort("127.0.0.1:1235")))
 	})
 
 	defer timer.Stop()
