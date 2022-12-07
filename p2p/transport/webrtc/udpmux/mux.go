d7b4ced5ec737e1886961927a439d55c5559502d
Chinmay Kousik <chinmaykousik1@gmail.com>
2022-12-07 18:22:37 +0530

    more comments

--- p2p/transport/webrtc/udpmux/mux.go
+++ p2p/transport/webrtc/udpmux/mux.go
@@ -23,11 +23,16 @@ type ufragConnKey struct {
 }
 
 // udpMux multiplexes multiple ICE connections over a single net.PacketConn,
-// generally a UDP socket. The connections are indexed by (ufrag, IP address type)
+// generally a UDP socket. The connections are indexed by (ufrag, IP address family)
 // and by remote address from which the connection has received valid STUN/RTC
 // packets. When a new packet is received on the underlying net.PacketConn, we
 // first check the address map to see if there is a connection associated with the
-// remote address. If found we forward the packet to the connection.
+// remote address. If found we forward the packet to the connection. If an associated
+// connection is not found, we check to see if the packet is a STUN packet. We then
+// fetch the ufrag of the remote from the STUN packet and use it to check if there
+// is a connection associated with the (ufrag, IP address family) pair. If found
+// we add the association to the address map. If not found, it is a previously
+// unseen IP address and the `unknownUfragCallback` callback is invoked.
 type udpMux struct {
 	mu                   sync.Mutex
 	wg                   sync.WaitGroup
