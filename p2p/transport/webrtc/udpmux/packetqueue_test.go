320d0474274933405059dddace4e5e206ac05b4b
Chinmay Kousik <chinmaykousik1@gmail.com>
2022-12-14 16:46:02 +0530

    use buffers from pool for connections

--- p2p/transport/webrtc/udpmux/packetqueue_test.go
+++ p2p/transport/webrtc/udpmux/packetqueue_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	pool "github.com/libp2p/go-buffer-pool"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,7 +17,7 @@ func TestPacketQueue_QueuePacketsForRead(t *testing.T) {
 	b.push([]byte{1, 2, 3}, net.UDPAddrFromAddrPort(netip.MustParseAddrPort("127.0.0.1:1234")))
 	b.push([]byte{5, 6, 7, 8}, net.UDPAddrFromAddrPort(netip.MustParseAddrPort("127.0.0.1:1235")))
 
-	buf := make([]byte, 100)
+	buf := pool.Get(100)
 	size, _, err := b.pop(buf)
 	require.NoError(t, err)
 	require.Equal(t, size, 3)
@@ -29,7 +30,7 @@ func TestPacketQueue_QueuePacketsForRead(t *testing.T) {
 func TestPacketQueue_WaitsForData(t *testing.T) {
 	ctx := context.Background()
 	pb := newPacketQueue(ctx)
-	buf := make([]byte, 100)
+	buf := pool.Get(100)
 
 	timer := time.AfterFunc(200*time.Millisecond, func() {
 		pb.push([]byte{5, 6, 7, 8}, net.UDPAddrFromAddrPort(netip.MustParseAddrPort("127.0.0.1:1235")))
@@ -39,4 +40,19 @@ func TestPacketQueue_WaitsForData(t *testing.T) {
 	size, _, err := pb.pop(buf)
 	require.NoError(t, err)
 	require.Equal(t, size, 4)
+}
+
+func TestPacketQueue_DropsPacketsWhenQueueIsFull(t *testing.T) {
+	ctx := context.Background()
+	pq := newPacketQueue(ctx)
+	addr := net.UDPAddrFromAddrPort(netip.MustParseAddrPort("127.0.0.1:12345"))
+	for i := 0; i < maxPacketsInQueue; i++ {
+		buf := pool.Get(255)
+		err := pq.push(buf, addr)
+		require.NoError(t, err)
+	}
+
+	buf := pool.Get(255)
+	err := pq.push(buf, addr)
+	require.ErrorIs(t, err, errTooManyPackets)
 }
