12492a2300697cbcbfdc8a9d10b5d8628c2ffa75
Marten Seemann <martenseemann@gmail.com>
2023-01-02 14:38:54 +1300

    swarm: use a sync.Pool to make metrics collection allocation-free

---
+++ p2p/net/swarm/swarm_metrics_test.go
@@ -0,0 +1,31 @@
+package swarm
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/network"
+
+	"github.com/stretchr/testify/require"
+)
+
+func BenchmarkMetricsConnOpen(b *testing.B) {
+	b.ReportAllocs()
+	quicConnState := network.ConnectionState{Transport: "quic"}
+	tcpConnState := network.ConnectionState{
+		StreamMultiplexer: "yamux",
+		Security:          "tls",
+		Transport:         "tcp",
+	}
+	_, pub, err := crypto.GenerateEd25519Key(rand.Reader)
+	require.NoError(b, err)
+	for i := 0; i < b.N; i++ {
+		switch i % 2 {
+		case 0:
+			recordConnectionOpened(network.DirInbound, pub, quicConnState)
+		case 1:
+			recordConnectionOpened(network.DirInbound, pub, tcpConnState)
+		}
+	}
+}
