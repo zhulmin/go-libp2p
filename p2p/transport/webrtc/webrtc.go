64b60d9ac15307d5b003cd023e1437062aba7e03
glendc <glen.decauwsemaecker@otainsight.com>
2023-01-19 23:40:37 +0100

    add link to go-libp2p webrtc spec

---
+++ p2p/transport/webrtc/webrtc.go
@@ -0,0 +1,5 @@
+// libp2pwebrtc implements the WebRTC transport for go-libp2p,
+// as officially described in https://github.com/libp2p/specs/tree/cfcf0230b2f5f11ed6dd060f97305faa973abed2/webrtc.
+//
+// TODO: give high level overview of spec and refer to the code in this (sub)module to see how we implement the spec.
+package libp2pwebrtc
