8423c5214d6e880647f3254ba3fb432fc8e755fe
glendc <glen.decauwsemaecker@otainsight.com>
2023-01-31 21:31:02 +0100

    sync SDP messages with Rust version

--- p2p/transport/webrtc/sdp.go
+++ p2p/transport/webrtc/sdp.go
@@ -11,8 +11,6 @@ import (
 
 // clientSDP describes an SDP format string which can be used
 // to infer a client's SDP offer from the incoming STUN message.
-// Note: ice-trickle is enabled by default on Pion, but in this
-// case it does not matter since we cannot exchange SDP or candidates.
 // The fingerprint used to render a client SDP is arbitrary since
 // it fingerprint verification is disabled in favour of a noise
 // handshake. The max message size is fixed to 16384 bytes.
@@ -22,9 +20,10 @@ o=- 0 0 IN %[1]s %[2]s
 s=-
 c=IN %[1]s %[2]s
 t=0 0
+
 m=application %[3]d UDP/DTLS/SCTP webrtc-datachannel
 a=mid:0
-a=ice-options:trickle
+a=ice-options:ice2
 a=ice-ufrag:%[4]s
 a=ice-pwd:%[4]s
 a=fingerprint:sha-256 ba:78:16:bf:8f:01:cf:ea:41:41:40:de:5d:ae:22:23:b0:03:61:a3:96:17:7a:9c:b4:10:ff:61:f2:00:15:ad
@@ -51,8 +50,7 @@ func renderClientSdp(addr *net.UDPAddr, ufrag string) string {
 // to infer the SDP answer of a server based on the provided
 // multiaddr, and the locally set ICE credentials. The max
 // message size is fixed to 16384 bytes.
-const serverSDP string = `
-v=0
+const serverSDP string = `v=0
 o=- 0 0 IN %[1]s %[2]s
 s=-
 t=0 0
@@ -64,10 +62,12 @@ a=ice-options:ice2
 a=ice-ufrag:%[4]s
 a=ice-pwd:%[4]s
 a=fingerprint:%[5]s
+
 a=setup:passive
 a=sctp-port:5000
 a=max-message-size:16384
 a=candidate:1 1 UDP 1 %[2]s %[3]d typ host
+a=end-of-candidates
 `
 
 func renderServerSdp(addr *net.UDPAddr, ufrag string, fingerprint *multihash.DecodedMultihash) (string, error) {
