135af0708130cea09960c47ea2bc787d08c7524c
glendc <glen.decauwsemaecker@otainsight.com>
2023-02-01 23:05:13 +0100

    remove outdated code and remove unneeded string alloc

--- p2p/transport/webrtc/string_util.go
+++ p2p/transport/webrtc/string_util.go
@@ -1,38 +1,7 @@
 package libp2pwebrtc
 
-import "strings"
+import "unsafe"
 
-func maFingerprintToSdp(fp string) string {
-	result := ""
-	first := true
-	for pos, char := range fp {
-		if pos%2 == 0 {
-			if first {
-				first = false
-			} else {
-				result += ":"
-			}
-		}
-		result += string(char)
-	}
-	return result
-}
-
-// intersperse string s with rune c at intervals of
-// length k
-func intersperse(s string, c rune, k int) string {
-	builder := &strings.Builder{}
-	builder.Grow(len(s) + len(s)/k)
-	for pos, ch := range s {
-		if pos%k == 0 && pos > 0 {
-			builder.WriteRune(c)
-		}
-		builder.WriteRune(ch)
-	}
-	return builder.String()
-}
-
-// assumption: all runes are ASCII
 func intersperse2(s string, c byte, k int) string {
 	m := len(s)
 	n := 0
@@ -45,5 +14,6 @@ func intersperse2(s string, c byte, k int) string {
 		buf[n] = b
 		n++
 	}
-	return string(buf[:n])
+	buf = buf[:n]
+	return *(*string)(unsafe.Pointer(&buf))
 }
--- p2p/transport/webrtc/util_test.go
+++ p2p/transport/webrtc/util_test.go
@@ -9,13 +9,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMaFingerprintToSdp(t *testing.T) {
-	certhash := "496612170D1C91AE574CC636DDD597D27D62C99A7FB9A3F47003E7439173235E"
-	expected := "49:66:12:17:0D:1C:91:AE:57:4C:C6:36:DD:D5:97:D2:7D:62:C9:9A:7F:B9:A3:F4:70:03:E7:43:91:73:23:5E"
-	result := maFingerprintToSdp(certhash)
-	require.Equal(t, expected, result)
-}
-
 func TestIntersperse2(t *testing.T) {
 	certhash := "496612170D1C91AE574CC636DDD597D27D62C99A7FB9A3F47003E7439173235E"
 	expected := "49:66:12:17:0D:1C:91:AE:57:4C:C6:36:DD:D5:97:D2:7D:62:C9:9A:7F:B9:A3:F4:70:03:E7:43:91:73:23:5E"
@@ -86,24 +79,10 @@ func TestRenderClientSDP(t *testing.T) {
 	require.Equal(t, expectedClientSDP, sdp)
 }
 
-func BenchmarkMaFingerprintToSdpIntersperse(b *testing.B) {
-	certhash := "496612170D1C91AE574CC636DDD597D27D62C99A7FB9A3F47003E7439173235E"
-	for i := 0; i < b.N; i++ {
-		maFingerprintToSdp(certhash)
-	}
-}
-
-func BenchmarkIntersperseSDP(b *testing.B) {
-	certhash := "496612170D1C91AE574CC636DDD597D27D62C99A7FB9A3F47003E7439173235E"
-	for i := 0; i < b.N; i++ {
-		intersperse(certhash, ':', 2)
-	}
-}
-
 func BenchmarkIntersperse2(b *testing.B) {
 	certhash := "496612170D1C91AE574CC636DDD597D27D62C99A7FB9A3F47003E7439173235E"
 	for i := 0; i < b.N; i++ {
-		intersperse2(certhash, byte(':'), 2)
+		intersperse2(certhash, ':', 2)
 	}
 }
 
