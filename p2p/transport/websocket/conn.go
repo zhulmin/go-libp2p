a1a35f5dc7bf37a177460edace5dbf11dc483e9f
Marco Munizaga <git@marcopolo.io>
2023-02-06 16:50:35 -0800

    Add comment

--- p2p/transport/websocket/conn.go
+++ p2p/transport/websocket/conn.go
@@ -30,7 +30,13 @@ func (c conn) Read(b []byte) (int, error) {
 	n, err := c.Conn.Read(b)
 	if err == nil && n == 0 && c.readAttempts < maxReadAttempts {
 		c.readAttempts++
-		// Nothing happened, let's read again.
+		// Nothing happened, let's read again. We reached the end of the frame
+		// we have
+		// (https://github.com/nhooyr/websocket/blob/master/netconn.go#L118).
+		// The next read will block until we get
+		// the next frame. We limit here to avoid looping in case of a bunch of
+		// empty frames.  Would be better if the websocket library did not
+		// return 0, nil here (see https://github.com/nhooyr/websocket/issues/367).  But until, then this is our workaround.
 		return c.Read(b)
 	}
 	return n, err
