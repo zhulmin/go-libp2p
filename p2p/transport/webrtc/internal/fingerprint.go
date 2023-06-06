e6d7d9f8c6f791a068132f9bc86f0c81ddd7922d
Marten Seemann <martenseemann@gmail.com>
2023-06-06 14:17:25 +0300

    webrtc: cleanup internal mess

--- p2p/transport/webrtc/internal/fingerprint.go
+++ p2p/transport/webrtc/internal/fingerprint.go
@@ -4,6 +4,13 @@ import (
 	"crypto"
 	"crypto/x509"
 	"errors"
+
+	"github.com/libp2p/go-libp2p/p2p/transport/webrtc/internal/encoding"
+
+	ma "github.com/multiformats/go-multiaddr"
+	"github.com/multiformats/go-multibase"
+	mh "github.com/multiformats/go-multihash"
+	"github.com/pion/webrtc/v3"
 )
 
 // Fingerprint is forked from pion to avoid bytes to string alloc,
@@ -23,4 +30,28 @@ func Fingerprint(cert *x509.Certificate, algo crypto.Hash) ([]byte, error) {
 	// https://golang.org/pkg/hash/#Hash
 	h.Write(cert.Raw)
 	return h.Sum(nil), nil
+}
+
+func DecodeRemoteFingerprint(maddr ma.Multiaddr) (*mh.DecodedMultihash, error) {
+	remoteFingerprintMultibase, err := maddr.ValueForProtocol(ma.P_CERTHASH)
+	if err != nil {
+		return nil, err
+	}
+	_, data, err := multibase.Decode(remoteFingerprintMultibase)
+	if err != nil {
+		return nil, err
+	}
+	return mh.Decode(data)
+}
+
+func EncodeDTLSFingerprint(fp webrtc.DTLSFingerprint) (string, error) {
+	digest, err := encoding.DecodeInterpersedHexFromASCIIString(fp.Value)
+	if err != nil {
+		return "", err
+	}
+	encoded, err := mh.Encode(digest, mh.SHA2_256)
+	if err != nil {
+		return "", err
+	}
+	return multibase.Encode(multibase.Base64url, encoded)
 }
