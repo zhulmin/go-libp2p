1b47936ac6eab765cb8de4037c41afffaf61d1f4
Marten Seemann <martenseemann@gmail.com>
2023-08-24 11:48:38 +0800

    webrtc: remove nonsensical comments in the muxed connection

--- p2p/transport/webrtc/udpmux/muxed_connection.go
+++ p2p/transport/webrtc/udpmux/muxed_connection.go
@@ -39,7 +39,15 @@ func (c *muxedConnection) Push(buf []byte) error {
 	return c.pq.Push(buf)
 }
 
-// Close implements net.PacketConn
+func (c *muxedConnection) ReadFrom(p []byte) (int, net.Addr, error) {
+	n, err := c.pq.Pop(c.ctx, p)
+	return n, c.addr, err
+}
+
+func (c *muxedConnection) WriteTo(p []byte, addr net.Addr) (n int, err error) {
+	return c.mux.writeTo(p, addr)
+}
+
 func (c *muxedConnection) Close() error {
 	select {
 	case <-c.ctx.Done():
@@ -51,40 +59,20 @@ func (c *muxedConnection) Close() error {
 	return nil
 }
 
-// LocalAddr implements net.PacketConn
-func (c *muxedConnection) LocalAddr() net.Addr {
-	return c.mux.socket.LocalAddr()
-}
+func (c *muxedConnection) LocalAddr() net.Addr { return c.mux.socket.LocalAddr() }
+func (c *muxedConnection) Address() net.Addr   { return c.addr }
 
-func (c *muxedConnection) Address() net.Addr {
-	return c.addr
-}
-
-// ReadFrom implements net.PacketConn
-func (c *muxedConnection) ReadFrom(p []byte) (int, net.Addr, error) {
-	n, err := c.pq.Pop(c.ctx, p)
-	return n, c.addr, err
-}
-
-// SetDeadline implements net.PacketConn
 func (*muxedConnection) SetDeadline(t time.Time) error {
 	// no deadline is desired here
 	return nil
 }
 
-// SetReadDeadline implements net.PacketConn
 func (*muxedConnection) SetReadDeadline(t time.Time) error {
 	// no read deadline is desired here
 	return nil
 }
 
-// SetWriteDeadline implements net.PacketConn
 func (*muxedConnection) SetWriteDeadline(t time.Time) error {
 	// no write deadline is desired here
 	return nil
-}
-
-// WriteTo implements net.PacketConn
-func (c *muxedConnection) WriteTo(p []byte, addr net.Addr) (n int, err error) {
-	return c.mux.writeTo(p, addr)
 }
